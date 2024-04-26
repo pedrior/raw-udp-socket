d2c5a3a87d218f208d35a8aae272fec203ce79d1
pedrior <pedrojdev@gmail.com>
2024-04-26 14:50:59 -0300

    melhora o menu

--- udp_client.py
+++ udp_client.py
@@ -7,34 +7,25 @@ from resources import *
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def parse_resource_type(option: int) -> int:
-    if option == 1:
-        return ResourceType.Datetime
-    elif option == 2:
-        return ResourceType.MotivationQuote
-    else:
-        return ResourceType.ServerStats
-
-
 while True:
     print('Bem-vindo ao cliente UDP!\n')
     option = int(input('Digite o tipo do recurso que deseja solicitar ao servidor:\n'
-                       '1 - Data e hora\n'
-                       '2 - Frase de motivação\n'
-                       '3 - Estatísticas do servidor\n'
-                       '4 - Sair\n\n'))
+                       '0 - Data e hora\n'
+                       '1 - Frase de motivação\n'
+                       '2 - Estatísticas do servidor\n'
+                       '3 - Sair\n\n'))
 
-    if option < 1 or option > 4:
+    if option < 0 or option > 3:
         print('Opção inválida. Tente novamente.\n')
         continue
 
-    if (option == 4):
+    if (option == 3):
         break
 
     print()
 
     # Cria uma mensagem de requisição com o tipo de recurso solicitado.
-    request = message.pack(parse_resource_type(option))
+    request = message.pack(ResourceType(option))
 
     # Envia a mensagem de requisição para o servidor.
     try:
